654478fd9e2617342f06f2f416abb755100b0ded
Ian Lucas Diaz <ianlucasdiaz95@gmail.com>
2019-10-30 10:57:29 -0300

    asdasd

--- tml.php
+++ tml.php
@@ -15,7 +15,7 @@ get_header();
 	<section id="primary" class="content-area">
 		<main id="main" class="site-main">
 
-			<div class="theme-my-login form">
+			<div class="theme-my-login form container">
 				<?php echo do_shortcode('[theme-my-login]') ?>
 			</div>
 
