9f08af0b76db97e999cfd5892d3f29b579b17928
Ian Lucas Diaz <ianlucasdiaz95@gmail.com>
2019-11-07 19:54:45 -0300

    asdasd

--- header.php
+++ header.php
@@ -76,6 +76,7 @@
 						$current_user = wp_get_current_user();
 						if ( ($current_user instanceof WP_User) ) {
 								echo get_avatar( $current_user->ID, 26 );
+								echo $current_user->name;
 						}
 					}else{ ?>
 						<a class="button login" href="<?php bloginfo('url') ?>/login">Ingresar</a>
