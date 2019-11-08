f4f2fb0b4081a07387a148ea9c4d3f522dc800aa
Ian Lucas Diaz <ianlucasdiaz95@gmail.com>
2019-11-08 15:04:12 -0300

    asdasd

--- header.php
+++ header.php
@@ -76,7 +76,7 @@
 						$current_user = wp_get_current_user();
 						if ( ($current_user instanceof WP_User) ) {
 								echo get_avatar( $current_user->ID, 26 );
-								?> <span class="current-username"><?php echo $current_user->display_name; ?></span> <a href="" class="button logout">Salir</a> <?php
+								?> <span class="current-username"><?php echo $current_user->display_name; ?></span> <a href="<?php echo wp_logout_url(); ?>" class="button logout">Salir</a> <?php
 						}
 					}else{ ?>
 						<a class="button login" href="<?php bloginfo('url') ?>/login">Ingresar</a>
