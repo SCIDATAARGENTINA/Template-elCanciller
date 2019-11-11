26a20156ce29f5dcc551289866e1ab72cb740295
Ian Lucas Diaz <ianlucasdiaz95@gmail.com>
2019-11-11 15:23:43 -0300

    asdasd

--- template-parts/dashboard/sidebar-dashboard.php
+++ template-parts/dashboard/sidebar-dashboard.php
@@ -14,7 +14,18 @@ $user = wp_get_current_user();
 ?>
 
 <div class="sidebarContent">
-    <div class="sidebar__avatar">
+    <div class="sidebar__profile">
         <img src="<?php echo get_avatar_url( $user->ID ) ?>" alt="<?php echo $user->name?>">
+        <div class="profile__user">
+            <div class="user__name">
+                <p><?php echo $user->name?></p>
+            </div>
+            <div class="user__actions">
+
+            </div>
+        </div>
+    </div>
+    <div class="sidebar__links">
+
     </div>
 </div>
