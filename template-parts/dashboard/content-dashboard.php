eb43bed95531624a7e4572d8587fbdfb19879606
Ian Lucas Diaz <ianlucasdiaz95@gmail.com>
2019-11-11 17:44:32 -0300

    asdasd

--- template-parts/dashboard/content-dashboard.php
+++ template-parts/dashboard/content-dashboard.php
@@ -16,7 +16,7 @@ $user = wp_get_current_user();
 
 <?php  
 
-    /* Check if user has favoritos
+    // Check if user has favoritos
     if(get_user_meta($user->ID, 'favoritos')){
       //Update favoritos con el nuevo fav
       $favoritos = get_user_meta($user->ID, 'favoritos', true);
@@ -29,9 +29,9 @@ $user = wp_get_current_user();
       add_user_meta($user->ID, 'favoritos', $favoritos);
     }
     echo $user->ID;
-    print_r(get_user_meta($user->ID, 'favoritos'));*/
+    print_r(get_user_meta($user->ID, 'favoritos'));
 
-    delete_user_meta( $user->ID, 'favoritos')
+    //delete_user_meta( $user->ID, 'favoritos')
  ?>
 </pre>
 <p> Dashboard Content </p>
