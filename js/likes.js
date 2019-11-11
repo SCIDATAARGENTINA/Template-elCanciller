d73b5870e6a476d10caf69d75ba9e2084093c8c8
Ian Lucas Diaz <ianlucasdiaz95@gmail.com>
2019-11-11 17:03:01 -0300

    asdasd

--- js/likes.js
+++ js/likes.js
@@ -6,7 +6,7 @@ let getPostData = (id, type) => {
         type = "posts";
     }
 
-    const url = `http://${window.location.host}/wp-json/wp/v2/${type}/${id}`;
+    const url = `http://142.93.24.13/wp-json/wp/v2/${type}/${id}`;
 
     return $.get(url);
 
