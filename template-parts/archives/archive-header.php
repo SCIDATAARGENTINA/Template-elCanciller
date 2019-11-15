dbef537bf9a8329145844b946d594fde17c1559f
Ian Lucas Diaz <ianlucasdiaz95@gmail.com>
2019-11-15 15:34:15 -0300

    asdasd

--- template-parts/archives/archive-header.php
+++ template-parts/archives/archive-header.php
@@ -52,7 +52,7 @@ $term = get_queried_object();
             <div class="archive-title" style="background: <?php echo $cat_color ?>">
                <div class="category">            
                   <h1><?php echo $categories[0]->name ?></h1>
-                  <?php if(is_user_logged_in()){ ?>
+                  <?php if( is_user_logged_in() ){ ?>
                      <?php if(checkIfFollowed('category', $categories[0]->term_id)){ ?>
                         <button data-type="category" data-id="<?php echo $categories[0]->term_id ?>" class="btn follow">Seguir</button>
                      <? }else{ ?>
