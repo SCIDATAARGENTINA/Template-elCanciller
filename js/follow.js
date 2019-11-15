1a737d71f9e65056d163b3c53c88344eb4e0e5bf
Ian Lucas Diaz <ianlucasdiaz95@gmail.com>
2019-11-15 14:59:07 -0300

    asdasdasd

--- js/follow.js
+++ js/follow.js
@@ -1,24 +1,30 @@
-let updateUserFollow = (itemId, itemType , url) => {
+jQuery(document).ready(function ($) {
 
-    $.ajax({
-        url: url,
-        type: 'POST',
-        data: {
-            'action': 'follow_author_category',
-            itemId,
-            itemType
-        },
-        success: function (result) {
-            //console.log(result);
-        },
-        error: function (errorThrown) {
-            //console.log(errorThrown);
-        }
+
+    let updateUserFollow = (itemId, itemType, url) => {
+
+        $.ajax({
+            url: url,
+            type: 'POST',
+            data: {
+                'action': 'follow_author_category',
+                itemId,
+                itemType
+            },
+            success: function (result) {
+                //console.log(result);
+            },
+            error: function (errorThrown) {
+                //console.log(errorThrown);
+            }
+        });
+
+    }
+
+    $('.follow').click(function () {
+        console.log(hola);
+        updateUserFollow($(this).attr('data-id'), $(this).attr('data-type'), follow.ajaxurl);
     });
 
-}
-
-$('.follow').click(function(){
-    console.log(hola);
-    updateUserFollow($(this).attr('data-id'), $(this).attr('data-type'), follow.ajaxurl );
+    
 });
