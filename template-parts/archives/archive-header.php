fc8c7d2093521da3eeb638296bc792da4dc43251
Ian Lucas Diaz <ianlucasdiaz95@gmail.com>
2019-11-15 15:37:13 -0300

    asdasd

--- template-parts/archives/archive-header.php
+++ template-parts/archives/archive-header.php
@@ -52,13 +52,11 @@ $term = get_queried_object();
             <div class="archive-title" style="background: <?php echo $cat_color ?>">
                <div class="category">            
                   <h1><?php echo $categories[0]->name ?></h1>
-                  <?php if( is_user_logged_in() ){ ?>
-                     <?php if(checkIfFollowed('category', $categories[0]->term_id)){ ?>
-                        <button data-type="category" data-id="<?php echo $categories[0]->term_id ?>" class="btn follow">Seguir</button>
-                     <? }else{ ?>
-                        <button data-type="category" data-id="<?php echo $categories[0]->term_id ?>" class="btn unfollow">Dejar de seguir</button>
-                     <? } ?>
-                  <?php } ?> 
+                  <?php if(checkIfFollowed('category', $categories[0]->term_id)){ ?>
+                     <button data-type="category" data-id="<?php echo $categories[0]->term_id ?>" class="btn follow">Seguir</button>
+                  <? }else{ ?>
+                     <button data-type="category" data-id="<?php echo $categories[0]->term_id ?>" class="btn unfollow">Dejar de seguir</button>
+                  <? } ?>
                </div>
                <div class="title">
                   <img src="<?php bloginfo('url') ?>/wp-content/uploads/2019/07/fire-blanco.svg" alt="">
