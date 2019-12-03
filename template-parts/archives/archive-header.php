e47cd203561e51f2b2f622344c1e8101607cf2f7
Ian Lucas Diaz <ianlucasdiaz95@gmail.com>
2019-12-03 16:44:02 -0300

    asdasd

--- template-parts/archives/archive-header.php
+++ template-parts/archives/archive-header.php
@@ -75,7 +75,7 @@ if ($count <= 0){
          <header class="archive-header">
             <div class="archive-title" style="background: <?php echo $cat_color ?>">
                <div class="category">            
-                  <h1><?php echo $categories[0]->name ?></h1>
+                  <h1><?php echo $term->name ?></h1>
                </div>
                <div class="title">
                   <img src="<?php bloginfo('url') ?>/wp-content/uploads/2019/07/fire-blanco.svg" alt="">
