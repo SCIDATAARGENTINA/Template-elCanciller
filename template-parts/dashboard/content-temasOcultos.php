c8f09a7a50554aa6bd0acadc0124cf7c046e24a3
Ian Lucas Diaz <ianlucasdiaz95@gmail.com>
2019-12-10 21:01:18 -0300

    asdasd

--- template-parts/dashboard/content-temasOcultos.php
+++ template-parts/dashboard/content-temasOcultos.php
@@ -14,19 +14,36 @@ $user = wp_get_current_user();
 ?>
 <div class="content__container"> 
   <div class="listItems">
-    <div class="listItems__header">
-      <h3>Las últimas notas que te gustaron</h3>
+    <div class="listItems__header -left">
+      <h3>Temas Ocultos</h3>
+      <p>Los temas que decidiste ocultar se muestran acá, si querés que se vuelvan a mostrar, selecciona "Dejar de ocultar"</p>
     </div>
-    <div class="listItems__posts">
+    <div class="listItems__hiddenTopics">
+      <div class="hiddenTopics">
+
 
       <?php
 
       $hidden_cats = get_user_meta( $user->ID, 'hidden_cats', true );
 
       print_r($hidden_cats);
+
+      foreach($hidden_cats as $cat_id){
+
+        $category = get_category( $cat_id );?>
+
+          <div class="hiddenTopic">
+
+            <h3><?php echo $category->name ?></h3>
+
+            <button class="btn unhide-category" data-categoryid="<?php echo $category->term_id ?>" data-categoryname="<?php echo $category->name ?>">Dejar de ocultar</button>
+
+          </div><!-- hiddenTopic -->
+      <?php }
       
       ?>
+      </div><!-- hiddenTopics -->
 
-    </div>
-  </div>
+    </div><!-- listItems__hiddenTopics -->
+  </div><!-- listItems -->
 </div><!-- content__container -->
