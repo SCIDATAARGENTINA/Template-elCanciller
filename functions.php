52a00a82064550b5d35bf805800cded20654c543
Ian Lucas Diaz <ianlucasdiaz95@gmail.com>
2020-01-03 16:58:26 -0300

    asdasd

--- functions.php
+++ functions.php
@@ -1703,6 +1703,8 @@ function checkIfFollowed($itemType, $itemId) {
 
     $authors = get_user_meta($user->ID, 'followed_authors', true);
     $in_array = array_search($itemId, $authors);
+    print_r($authors);
+    print_r($in_array);
     
     if($in_array){
       return true;
