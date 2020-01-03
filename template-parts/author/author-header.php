7321536c0e451a42ad20dc5c6ff26bf13a1ab7a8
Ian Lucas Diaz <ianlucasdiaz95@gmail.com>
2020-01-03 16:54:23 -0300

    asdasd

--- template-parts/author/author-header.php
+++ template-parts/author/author-header.php
@@ -22,7 +22,7 @@ $alt = get_post_meta($thumbnail_id, '_wp_attachment_image_alt', true);
             Por: <?php echo get_the_author_meta( 'display_name', $author_id ) ?>
          </h1>
          <div class="follow-container">
-            <?php if( is_user_logged_in() && is_category() ){ ?>
+            <?php if( is_user_logged_in() && is_author() ){ ?>
                <?php if( checkIfFollowed('author', $author_id)){  // Si es true es que sigue la categoria?>
                   <button data-type="category" data-id="<?php echo $author_id ?>" class="btn follow -isFollowed -accent">Dejar de seguir</button>
                <?php }else{ //Si es false no sigue la categoria?>
