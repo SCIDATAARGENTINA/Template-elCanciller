1ac5238e4ebb332ba092f3992eeb71ca20d655c2
Ian Lucas Diaz <ianlucasdiaz95@gmail.com>
2020-01-09 14:57:55 -0300

    asdasd

--- js/scripts.js
+++ js/scripts.js
@@ -215,9 +215,13 @@ jQuery(document).ready(function($) {
         var top_of_screen = $(window).scrollTop();
 
         if ((bottom_of_screen > top_of_element) && (top_of_screen < bottom_of_element)) {
-            console.log('se ve');
+
+            $('.sidebar__content').addClass('-hidden');
+            
         } else {
-            // the element is not visible, do something else
+            
+            $('.sidebar__content').removeClass('-hidden');
+
         }
     });
 
