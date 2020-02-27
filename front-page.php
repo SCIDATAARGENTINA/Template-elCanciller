2ba09a2fd6305dea9daff6f7b325a5c3e3619c41
Lucas <lucas@scidata.com.ar>
2020-02-27 00:04:17 -0300

    abc01

--- front-page.php
+++ front-page.php
@@ -42,11 +42,11 @@ minuto = n.getMinutes();
 segundo = n.getSeconds();
 
 fechaactual = Date.parse(y + "-" + m + "-" + d + " " + h + ":" + minuto + ":" + segundo);
-fechainicio = Date.parse('2020-02-27 00:00:00');
-fechafin = Date.parse('2020-02-28 00:00:00');
+fechainicio = Date.parse('2020-03-01 00:00:00');
+fechafin = Date.parse('2020-03-02 00:00:00');
 if (fechaactual > fechainicio && fechafin > fechaactual)
 {
-jQuery('#new').append('<img class=\"onlydesktop\" style=\"margin: 0 auto;padding-top: 2%;\" src=\"http://142.93.24.13/wp-content/uploads/2020/02/apertura-desktop.jpg\"><img class=\"onlymobile\" src=\"http://142.93.24.13/wp-content/uploads/2020/02/apertura-mobile.jpg\" class=\"onlymobile\" style=\"margin: 0 auto;padding-top: 2%;\">');
+jQuery('#new').append('<img class=\"onlydesktop\" style=\"margin: 0 auto;padding-top: 2%;\" src=\"https://elcanciller.com/wp-content/uploads/2020/02/apertura-desktop.jpg\"><img class=\"onlymobile\" src=\"https://elcanciller.com/wp-content/uploads/2020/02/apertura-mobile.jpg\" class=\"onlymobile\" style=\"margin: 0 auto;padding-top: 2%;\">');
 }
 });
 				</script>
