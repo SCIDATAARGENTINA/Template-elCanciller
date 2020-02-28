21e9ed43700e74f5e1363e6932de8f0e4e3b5eff
Lucas <lucas@scidata.com.ar>
2020-02-28 11:54:33 -0300

    b1

--- template-parts/widgets/widget-cancilleram.php
+++ template-parts/widgets/widget-cancilleram.php
@@ -18,7 +18,7 @@
      <?php
    $args = array(
       'post_type' => 'cancilleram',
-      'posts_per_page' => 10,
+      'posts_per_page' => 11,
       'orderby' => 'date',
       'order' => 'ASC',
    );
