5936f725ed4b1b8ced8da7d50b4158d48d4c36cc
Lucas <lucas@scidata.com.ar>
2020-07-03 12:59:57 -0300

    autoreslateral

--- template-parts/widgets/widget-autores.php
+++ template-parts/widgets/widget-autores.php
@@ -16,28 +16,36 @@
 <div class="instagram-widget container">
         <div class="autores-profile">
             <div class="autores-pic">
-                <a href="<?php bloginfo('url') ?>/author/jesicabossi/"><img src="<?php bloginfo('url') ?>/wp-content/uploads/2020/01/jesica-bossi-lateral.png" alt="Jesica Bossi"></a>
-                <div class="autores-data">
-                    <h4><a href="<?php bloginfo('url') ?>/author/jesicabossi/">Jesica Bossi</a></h4>
-                    
-                </div>
-            </div>
-            <div class="autores-pic">
-                <a href="<?php bloginfo('url') ?>/author/marianoojeda/"><img src="<?php bloginfo('url') ?>/wp-content/uploads/2019/09/marianoojeda.png" alt="Mariano Ojeda"></a>
-                <div class="autores-data">
-                    <h4><a href="<?php bloginfo('url') ?>/author/marianoojeda/">Mariano Ojeda</a></h4>
-                    
-                </div>
-            </div>
-            <div class="autores-pic">
                 <a href="<?php bloginfo('url') ?>/author/diegogenoud/"><img src="<?php bloginfo('url') ?>/wp-content/uploads/2019/09/Diego-Genoud-Autor.png" alt="Diego Genoud"></a>
                 <div class="autores-data">
                     <h4><a href="<?php bloginfo('url') ?>/author/diegogenoud/">Diego Genoud</a></h4>
                     
                 </div>
             </div>
+            <div class="autores-pic">
+                <a href="<?php bloginfo('url') ?>/author/martinrodriguez/"><img src="<?php bloginfo('url') ?>/wp-content/uploads/2020/06/martin-rodriguez-lateral-ok.png" alt="Martin Rodriguez"></a>
+                <div class="autores-data">
+                    <h4><a href="<?php bloginfo('url') ?>/author/martinrodriguez/">Martín Rodriguez</a></h4>
+                    
+                </div>
+            </div>
+            <div class="autores-pic">
+                <a href="<?php bloginfo('url') ?>/author/luciaaisicoff/"><img src="<?php bloginfo('url') ?>/wp-content/uploads/2020/06/lucia-aisicoff-lateral.png" alt="Lucía Aisicoff"></a>
+                <div class="autores-data">
+                    <h4><a href="<?php bloginfo('url') ?>/author/luciaaisicoff/">Lucía Aisicoff</a></h4>
+                    
+                </div>
+            </div>
+            
         </div>
         <div class="autores-profile">
+            <div class="autores-pic">
+                <a href="<?php bloginfo('url') ?>/author/eddiefitte/"><img src="<?php bloginfo('url') ?>/wp-content/uploads/2020/06/eddie-fitte-lateral.png" alt="Eddie Fitte"></a>
+                <div class="autores-data">
+                    <h4><a href="<?php bloginfo('url') ?>/author/eddiefitte/">Eddie Fitte</a></h4>
+                    
+                </div>
+            </div>
             <div class="autores-pic">
                 <a href="<?php bloginfo('url') ?>/author/solrodriguezgarnica/"><img src="<?php bloginfo('url') ?>/wp-content/uploads/2019/09/Sol-Rodriguez-Garnica-Autora.png" alt="Sol Rodriguez Garnica"></a>
                 <div class="autores-data">
@@ -46,16 +54,32 @@
                 </div>
             </div>
             <div class="autores-pic">
-                <a href="<?php bloginfo('url') ?>/author/juan-jose-dominguez/"><img src="<?php bloginfo('url') ?>/wp-content/uploads/2019/11/Juan-Jose-Dominguez-Autor.png" alt="Juan José Dominguez"></a>
+                <a href="<?php bloginfo('url') ?>/author/augustotaglioni/"><img src="<?php bloginfo('url') ?>/wp-content/uploads/2020/06/augusto-taglioni-lateral-ok.png" alt="Augusto Taglioni"></a>
                 <div class="autores-data">
-                    <h4><a href="<?php bloginfo('url') ?>/author/juan-jose-dominguez/">J. José Dominguez</a></h4>
+                    <h4><a href="<?php bloginfo('url') ?>/author/augustotaglioni/">Augusto Taglioni</a></h4>
+                    
+                </div>
+            </div>
+        </div>
+        <div class="autores-profile">
+            <div class="autores-pic">
+                <a href="<?php bloginfo('url') ?>/author/federicozinni/"><img src="<?php bloginfo('url') ?>/wp-content/uploads/2020/07/federico-zinni-lateral.png" alt="Federico Zinni"></a>
+                <div class="autores-data">
+                    <h4><a href="<?php bloginfo('url') ?>/author/federicozinni/">Federico Zinni</a></h4>
                     
                 </div>
             </div>
             <div class="autores-pic">
-                <a href="<?php bloginfo('url') ?>/author/ayelenoliva/"><img src="<?php bloginfo('url') ?>/wp-content/uploads/2019/11/Ayelen-Oliva.png" alt="Ayelén Oliva"></a>
+                <a href="<?php bloginfo('url') ?>/author/anaclaraperezcotten/"><img src="<?php bloginfo('url') ?>/wp-content/uploads/2020/07/ana-clara-perez-cotten-lateral.png" alt="Ana Clara Pérez Cotten"></a>
                 <div class="autores-data">
-                    <h4><a href="<?php bloginfo('url') ?>/author/ayelenoliva/">Ayelén Oliva</a></h4>
+                    <h4><a href="<?php bloginfo('url') ?>/author/anaclaraperezcotten/">Ana Clara P. Cotten</a></h4>
+                    
+                </div>
+            </div>
+            <div class="autores-pic">
+                <a href="<?php bloginfo('url') ?>/author/paulapuebla/"><img src="<?php bloginfo('url') ?>/wp-content/uploads/2020/07/paula-puebla-lateral.png" alt="Paula Puebla"></a>
+                <div class="autores-data">
+                    <h4><a href="<?php bloginfo('url') ?>/author/paulapuebla/">Paula Puebla</a></h4>
                     
                 </div>
             </div>
